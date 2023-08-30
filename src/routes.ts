ffc89b17ccda4c4f9120fcb26bd765b46e3ae2c2
ViniciusU <vini.gama.15@gmail.com>
2023-08-30 10:40:27 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { request, response, Router } from "express";
 import {CreateUserController} from "./modules/Acount/CreateUser/CreateUserControllers"
 import {AuthenticateClientController} from "./modules/Acount/Authenticate/AuthenticateUserControllers"
 import {ensureAuthenticateUser} from "./middlewares/ensureUserAuthenticate"
+import { prisma } from "./database/prismaClient";
 
 const routes = Router();
 const createUserController = new CreateUserController(); 
@@ -10,6 +11,14 @@ const authenticateClientController = new AuthenticateClientController();
 
 routes.post("/create/user",(request, response)=>  createUserController.handle(request, response) )
 routes.post("/authenticate",(request, response)=>  authenticateClientController.handle(request, response) )
+
+routes.get('/users', async (request, response) => {
+  
+  
+    const users = await prisma.user.findMany()
+  
+    return response.json(users)
+  })
 
 
 
